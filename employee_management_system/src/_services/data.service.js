f55ee2e4497f7e8a9ded5dc507788189ca278675
semere01 <se.semere.tereffe@gmail.com>
2021-06-14 16:52:09 +0300

    patching front end and back end

--- employee_management_system/src/_services/data.service.js
+++ employee_management_system/src/_services/data.service.js
@@ -8,12 +8,12 @@ export const dataService = {
   getDepartments,
 };
 
-const token =
+let token =
   (JSON.parse(localStorage.getItem("currentUser")) &&
     JSON.parse(localStorage.getItem("currentUser")).token) ||
   " ";
 
-const config = {
+let config = {
   headers: {
     "x-access-token": token,
   },
@@ -21,17 +21,35 @@ const config = {
 };
 
 function getEmployees(username, password) {
+
   return axios({ url: "/employees", method: "GET", ...config }).then(
     (response) => {
       return response.data;
     }
   );
 }
-
+function getBonusCuts() {
+  // console.log(`SALARYYY\n\n\n${getSalary(34)}`)
+  return axios({ url: "/bonus_cuts", method: "GET", ...config }).then(
+    (response) => {
+      console.log(response.data);
+    }
+  );
+}
+function getSalary(employee_id){
+  return axios({ url: "/salary", method: "GET", ...config }).then(
+    (response) => {
+      console.log(response.data);
+    }
+  );
+}
 function getDepartments() {
+  console.log(`SALARYYY\n\n\n${getBonusCuts()}`)
   return axios({ url: "/departments", method: "GET", ...config }).then(
     (response) => {
       return response.data;
     }
   );
 }
+
+console.log("I AM IN DATA.SERVICE.JS");
