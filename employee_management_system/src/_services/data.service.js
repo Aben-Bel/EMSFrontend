e98ed8605553b2982e8ec7eb24bd639930fee0c5
aben-bel <abenezer.sbelay@gmail.com>
2021-06-15 15:12:39 +0300

    fix expire date

--- employee_management_system/src/_services/data.service.js
+++ employee_management_system/src/_services/data.service.js
@@ -8,48 +8,76 @@ export const dataService = {
   getDepartments,
 };
 
-let token =
-  (JSON.parse(localStorage.getItem("currentUser")) &&
-    JSON.parse(localStorage.getItem("currentUser")).token) ||
-  " ";
+// let token =
+//   (JSON.parse(localStorage.getItem("currentUser")) &&
+//     JSON.parse(localStorage.getItem("currentUser")).token) ||
+//   " ";
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    // Do something with response error
+    console.log("Error: ", error.response);
+    try {
+      if (
+        error.response ||
+        error.response.data.message === "Token is invalid"
+      ) {
+        authenticationService.logout();
+      }
+    } catch {}
+
+    return Promise.reject(error);
+  }
+);
 
 let config = {
-  headers: {
-    "x-access-token": token,
-  },
   baseURL: "http://127.0.0.1:5000/",
 };
 
 function getEmployees(username, password) {
-
-  return axios({ url: "/employees", method: "GET", ...config }).then(
-    (response) => {
-      return response.data;
-    }
-  );
+  return axios({
+    url: "/employees",
+    method: "GET",
+    headers: { "x-access-token": authenticationService.currentUserValue.token },
+    ...config,
+  }).then((response) => {
+    return response.data;
+  });
 }
 function getBonusCuts() {
   // console.log(`SALARYYY\n\n\n${getSalary(34)}`)
-  return axios({ url: "/bonus_cuts", method: "GET", ...config }).then(
-    (response) => {
-      console.log(response.data);
-    }
-  );
+  return axios({
+    url: "/bonus_cuts",
+    method: "GET",
+    headers: { "x-access-token": authenticationService.currentUserValue.token },
+    ...config,
+  }).then((response) => {
+    console.log(response.data);
+  });
 }
-function getSalary(employee_id){
-  return axios({ url: "/salary", method: "GET", ...config }).then(
-    (response) => {
-      console.log(response.data);
-    }
-  );
+function getSalary(employee_id) {
+  return axios({
+    url: "/salary",
+    method: "GET",
+    headers: { "x-access-token": authenticationService.currentUserValue.token },
+    ...config,
+  }).then((response) => {
+    console.log(response.data);
+  });
 }
 function getDepartments() {
-  console.log(`SALARYYY\n\n\n${getBonusCuts()}`)
-  return axios({ url: "/departments", method: "GET", ...config }).then(
-    (response) => {
-      return response.data;
-    }
-  );
+  console.log(`SALARYYY\n\n\n${getBonusCuts()}`);
+  return axios({
+    url: "/departments",
+    method: "GET",
+    headers: { "x-access-token": authenticationService.currentUserValue.token },
+    ...config,
+  }).then((response) => {
+    return response.data;
+  });
 }
 
 console.log("I AM IN DATA.SERVICE.JS");
